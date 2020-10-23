ee3673f7abaea58d01f5c989da8b0868b346f114
Sarah Peters <sarahnpeters@gmail.com>
2020-10-23 17:36:45 -0500

    creating PUT route on server side and confirming it works on postman

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -18,7 +18,24 @@ router.get('/', (req, res) => {
     });
 });
 
-//POST route
+//POST route (takes the input from client and inserts task into db. then GET to give list of tasks from db to client)
+router.post('/', (req, res) => {
+  const todoData = req.body;
+  const queryText = `INSERT INTO "todo" ("task", "status")
+    VALUES ($1, 'false');`;
+
+  const queryArray = [todoData.task];
+
+  pool
+    .query(queryText, queryArray)
+    .then((dbResponse) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 //UPDATE route
 
