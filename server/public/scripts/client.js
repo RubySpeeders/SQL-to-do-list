101abdad756c8297b753e05c4d195903f9d51c1a
Sarah Peters <sarahnpeters@gmail.com>
2020-10-23 21:59:44 -0500

    adding html elements for input and table for tasks. adding click listener to submit task. adding GET and POST routes on client side and render function to append info on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,58 @@
-console.log('js');
-
 $(document).ready(readyUp);
 
 function readyUp() {
-  console.log('jq');
+  //click listener for submit button
+  $('#js-addBtn').on('click', handleClickAdd);
+  getTasks();
+}
+
+function handleClickAdd() {
+  const taskObj = {
+    task: $('#js-task').val(),
+  };
+  postTask(taskObj);
+}
+
+//GET function to get the list of tasks from db
+function getTasks() {
+  $.ajax({
+    type: 'GET',
+    url: '/todo',
+  })
+    .then((response) => {
+      render(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+//POST function to send object to db
+function postTask(obj) {
+  $.ajax({
+    type: 'POST',
+    url: '/todo',
+    data: obj,
+  })
+    .then((response) => {
+      //clearForm();
+      //db is updated, update DOM
+      getTasks();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function render(response) {
+  $('.js-tasks').empty();
+  for (let task of response) {
+    $('.js-tasks').append(
+      `<tr>
+        <td>${task.task}</td>
+        <td><button>DONE</button></td>
+        <td><button>DELETE</button></td>
+      </tr>`
+    );
+  }
 }
